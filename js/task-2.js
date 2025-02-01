f36733fb8786fdd47f9e59f969e4b634fcf02e34
vladshein <vlshein1@gmail.com>
2025-02-01 22:27:32 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+// Задача 2. Склад
+
+class Storage {
+  #items = [];
+
+  constructor(array) {
+    this.#items = [...array];
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    let index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
